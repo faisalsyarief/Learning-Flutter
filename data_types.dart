ef2a850a6223dfeb25ce224e02a3d819936d7d8e
faisalsyarief <ichaltkj96@gmail.com>
2024-11-16 21:42:54 +0700

    data type func

---
+++ data_types.dart
@@ -0,0 +1,17 @@
+void main() {
+  var val;
+  val = 999;
+  val = 'Flutter';
+  print(val);
+}
+
+// Output:
+// Flutter
+
+// ========== //
+// Code salah
+// void main() {
+//   var val = 999;
+//   val = 'Flutter'; // Kesalahan assignment
+//   print(x);
+// }
