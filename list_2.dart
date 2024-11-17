1569d1ee97729d54180fb74c6491d75cdd6f4a12
faisalsyarief <ichaltkj96@gmail.com>
2024-11-17 20:21:34 +0700

    list func

---
+++ list_2.dart
@@ -0,0 +1,12 @@
+void main() {
+  List<String> listData = ['Flutter 1', 'Flutter 2', 'Flutter 3', 'Flutter 4', 'Flutter 5'];
+
+  listData.forEach((s) => print(s));
+}
+
+// Output:
+// Flutter 1
+// Flutter 2
+// Flutter 3
+// Flutter 4
+// Flutter 5
