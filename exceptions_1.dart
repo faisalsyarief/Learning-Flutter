c4a9d065b245b0524fc12b3f5e93bf05e48c4c29
faisalsyarief <ichaltkj96@gmail.com>
2024-12-26 13:47:08 +0700

    Exception

---
+++ exceptions_1.dart
@@ -0,0 +1,12 @@
+void main() {
+  try {
+    var val1 = 99;
+    var val2 = 0;
+    print(val1 ~/ val2);
+  } catch (e) {
+    print('Exception: $e');
+  }
+}
+
+// Output:
+// Exception: IntegerDivisionByZeroException
