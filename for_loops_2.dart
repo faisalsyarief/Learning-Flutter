c2310fc0ddbe7b272229f75136c49589e5331979
faisalsyarief <ichaltkj96@gmail.com>
2025-01-02 20:57:09 +0700

    for loops

---
+++ for_loops_2.dart
@@ -0,0 +1,17 @@
+void main() {
+  for (int i = 0; i < 10; i++) {
+    print('hello ${i + 1}');
+  }
+}
+
+// Output:
+// hello 1
+// hello 2
+// hello 3
+// hello 4
+// hello 5
+// hello 6
+// hello 7
+// hello 8
+// hello 9
+// hello 10
