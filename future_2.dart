26d59301d20727ebb6bb5624adf1893ffd380d05
faisalsyarief <ichaltkj96@gmail.com>
2025-01-09 16:40:59 +0700

    future

---
+++ future_2.dart
@@ -0,0 +1,16 @@
+void main() {
+  getOrder().then((value) {
+    print('Your ordered: $value');
+  });
+  print('Getting your order...');
+}
+
+Future<String> getOrder() {
+  return Future.delayed(Duration(seconds: 3), () {
+    return 'Coffee Boba';
+  });
+}
+
+// Output:
+// Getting your order...
+// Your ordered: Coffee Boba
